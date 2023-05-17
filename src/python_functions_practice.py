f251cc4eaf13399b4d34a84b3c2bb3172b95d772
Kirsty McBride <kirsty@Kirstys-MacBook-Pro.local>
2023-05-17 15:51:53 +0100

    9 passed

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -15,3 +15,26 @@ def divide (x, y):
 
 def length_of_string(string):
     return len(string)
+
+def join_string(x, y):
+    return x + y
+
+def add_string_as_number(x, y):
+    return int(x) + int(y)
+
+def number_to_full_month_name(x):
+    months = {
+        1 : "January",
+        2 : "February",
+        3 : "March",
+        4 : "April",
+        5 : "May",
+        6 : "June",
+        7 : "July",
+        8 : "August",
+        9 : "September",
+        10 : "October",
+        11 : "November",
+        12 : "Decemeber"
+    }
+    return months [x]
